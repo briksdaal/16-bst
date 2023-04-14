6878ced268725a721a8c93b1ee363d4a8785e6dd
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-04-15 01:30:44 +0300

    Create project files

---
+++ README.md
@@ -0,0 +1,15 @@
+Odin Binary Search Trees Assignment
+
+Implement a binary search tree data structure built from an array. BST includes a balancing mechanism, and supports the following actions:
+buildTree
+insert
+delete
+find
+levelOrder
+inorder
+preorder
+postorder
+height
+depth
+isBalanced
+rebalance
---
+++ index.js
@@ -0,0 +1,17 @@
+import printBST from './bst.js';
+import printNode from './node.js';
+
+printBST();
+printNode();
+
+// Write a simple driver script that does the following:
+// 1. Create a binary search tree from an array of random numbers.
+// You can create a function that returns an array of random numbers
+// every time you call it, if you wish.
+// 2. Confirm that the tree is balanced by calling isBalanced
+// 3. Print out all elements in level, pre, post, and in order
+// 4. Unbalance the tree by adding several numbers > 100
+// 5. Confirm that the tree is unbalanced by calling isBalanced
+// 6. Balance the tree by calling rebalance
+// 7. Confirm that the tree is balanced by calling isBalanced
+// 8. Print out all elements in level, pre, post, and in order
